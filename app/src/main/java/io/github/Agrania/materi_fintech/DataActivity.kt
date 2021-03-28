b68c266a4808ccffab35ae59add1f55fa9a04e2c
Agrania <arapriskaz@gmail.com>
2021-03-28 13:25:53 +0700

    Initial Project

---
+++ app/src/main/java/io/github/Agrania/materi_fintech/DataActivity.kt
@@ -0,0 +1,11 @@
+package io.github.Agrania.materi_fintech
+
+import android.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+    }
+}
